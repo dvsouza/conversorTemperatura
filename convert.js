2789257611e60579099e34841c11f73a111ad92c
Diogo Souza <diogovilela04@gmail.com>
2021-10-12 00:05:47 -0300

    Estruturando código javascript

--- convert.js
+++ convert.js
@@ -1,10 +1,40 @@
-var botaoConversor = document.querySelector(".converte");
-var temperaturaSelecionada = document.querySelector("#seleciona-temperatura").value;
+// var botaoConversor = document.querySelector(".converte");
+var temperaturaInformada = document.querySelector("#insere-temperatura");
+var selectValue = null;
+var fahrenheit = null;
+
+function getSelectValue(){
+    selectValue = document.getElementById("seleciona-temperatura").value;
+    return selectValue;
+}
+
+function calculaConversao(temperatura){
+        fahrenheit = temperatura.value * 1.8 + 32;
+        return fahrenheit;
+}
+
+function criaElemento(){
+    var divResultado = document.querySelector(".valorConvertido");
+    var resultadoConvertido = document.createElement("p");
+    resultadoConvertido.setAttribute("class", "resultado");
+    var elementoResultado = divResultado.appendChild(resultadoConvertido);
+    return elementoResultado;
+}
+
+function insereNaTela(){
+    var retornoTela = "A temperatura em " + selectValue + " é de " + calculaConversao(temperaturaInformada);
+    var novaTemperatura = criaElemento();
+    novaTemperatura.innerHTML = retornoTela;
+}
 
 
+// botaoConversor.addEventListener("click", function(event){
+//     event.preventDefault();
+//     insereNaTela();
+// })
 
-botaoConversor.addEventListener("click", function(event){
+function botaoConversor(){
     event.preventDefault();
-    console.log("Fui Clicado!")
-    console.log(temperaturaSelecionada)
-})
+    insereNaTela();
+}
+
